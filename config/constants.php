e42e3d27e5765d7414b5f15de1e3d5e592ce8db3
LuisMM24 <luismateo2416@gmail.com>
2022-01-27 23:24:56 +0100

    requires in index

--- config/constants.php
+++ config/constants.php
@@ -1,10 +1,16 @@
 <?php
 require_once("constantsBase.php");
+require_once("db.php");
 //constants declaration
 
 define("MODELS", BASE_PATH . "/src/models/");
+
 define("CONTROLLERS", BASE_PATH . "/src/models/");
+
 define("LIBS", BASE_PATH . "/src/libs/");
+
 define("TEMPLATES", BASE_PATH . "/assets/templates/");
+
 define("VIEWS", BASE_PATH . "/src/views/");
+
 define("ASSETS", BASE_URL . "/assets/");
--- index.php
+++ index.php
@@ -1,2 +1,13 @@
 <?php
+//constants
 require_once("config/constants.php");
+//My app
+require_once(LIBS . "router.php");
+//database class
+require_once(LIBS . "database.php");
+//controller class template
+require_once(LIBS . "classes/controller.php");
+//model class template
+require_once(LIBS . "classes/model.php");
+//view class
+require_once(LIBS . "classes/view.php");
